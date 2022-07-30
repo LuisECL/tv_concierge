50f962f5c4f28eed0b46fb391fa1fcd0a47ed0c6
Luis <lecarcamol@gmail.com>
2022-07-30 10:42:41 -0300

    feat: Search on keyup
    Search bar calling the search function every time the keyup event is launched

---
+++ script.js
@@ -0,0 +1,13 @@
+let searchBar = document.getElementById('search-bar')
+
+function search(text){
+  fetch(`http://www.omdbapi.com/?apikey=ef1d1c7c&s=${text}`)
+    .then(promise => promise.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+searchBar.addEventListener("keyup", text => {
+  console.log(searchBar.value);
+  search(searchBar.value)
+})
