cbc36da4eff1cd2764aa9625102ec3417cf71ff9
Luis <lecarcamol@gmail.com>
2022-07-30 12:29:40 -0300

    fix: Clear search result
    Added line 16 at script.js to clear the results list

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let searchContainer = document.querySelector(".search-container")
 let searchBar = document.getElementById('search-bar')
+let results = document.querySelector(".results")
 
 function search(text){
   fetch(`http://www.omdbapi.com/?apikey=ef1d1c7c&s=${text}`)
     .then(promise => promise.json())
     .then(data => {
-      addResult(data)
+      showSearchResults(data)
     })
     .catch(err => console.log(err))
 }
 
-function addResult(result){
-  let searchResult = document.createElement("div");
-  searchResult.classList.add("result-container");
-  searchResult.innerHTML = `
-  <img src="${result.Search[1].Poster}" alt="">
-  <p>${result.Search[1].Title}</p>
-  `
-  searchContainer.appendChild(searchResult)
+function showSearchResults(response){
+  console.log(results)
+  results.innerHTML = ""
+
+  if(response.Response){
+    console.log(response)
+
+    for (let i=0; i<6; i++){
+      let resultContainer = document.createElement("div")
+      resultContainer.classList.add("result-container")
+      resultContainer.innerHTML = `
+      <img src="${response.Search[i].Poster}" alt="">
+      <p>${response.Search[i].Title}</p>
+      `
+      results.appendChild(resultContainer)
+    }
+
+  } else {
+    results.innerHTML = ""
+  }
 }
 
 searchBar.addEventListener("keyup", () => {
