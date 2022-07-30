0566a2945b5877b74c4567a715f18a237851a31e
Luis <lecarcamol@gmail.com>
2022-07-30 11:57:38 -0300

    feat: Add results to search bar
    search and addResult functions working, however, it needs to eliminate elements after search is emptied

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+let searchContainer = document.querySelector(".search-container")
 let searchBar = document.getElementById('search-bar')
 
 function search(text){
   fetch(`http://www.omdbapi.com/?apikey=ef1d1c7c&s=${text}`)
     .then(promise => promise.json())
-    .then(data => console.log(data))
+    .then(data => {
+      addResult(data)
+    })
     .catch(err => console.log(err))
+}
+
+function addResult(result){
+  let searchResult = document.createElement("div");
+  searchResult.classList.add("result-container");
+  searchResult.innerHTML = `
+  <img src="${result.Search[1].Poster}" alt="">
+  <p>${result.Search[1].Title}</p>
+  `
+  searchContainer.appendChild(searchResult)
 }
 
 searchBar.addEventListener("keyup", () => {
